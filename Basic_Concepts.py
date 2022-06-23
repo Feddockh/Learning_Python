bb8117f1842144e05ac1bca4d9ac8b0244aaadf3
Feddockh <Hayden4feddock@gmail.com>
2022-06-22 22:08:37 -0400

    Basic Concepts Complete

---
+++ Basic_Concepts.py
@@ -0,0 +1,14 @@
+# Use the print() function to output text
+print("example")
+
+# Division results in floats, operations across datatypes will defualt to the larger
+
+# Exponentiation can be performed using the ** operator
+print("2**5 = ", 2**5)
+
+# Floor division can be performed using the // operator
+print ("9//2 = ", 9//2)
+
+# Remainder can be found using the % operator
+print("3%2 = ", 3%2)
+
---
+++ Strings_And_Variables.py
@@ -0,0 +1,7 @@
+# Both double and single quotes can be used to designate strings
+print("String with double quotes")
+print('String with single quotes')
+
+# Escaping characters can be preformed by using a backslash \
+print ('Using Python\'s escape sequences')
+
